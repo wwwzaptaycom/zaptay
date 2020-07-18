e3338fd4ddc08f5b30af316376d4e068cf1caf94
wwwzaptaycom <admin@zaptay.com>
2020-07-18 19:17:53 +0530

    Home page modification done

--- category/urls.py
+++ category/urls.py
@@ -7,5 +7,5 @@ app_name = 'category'
 
 urlpatterns = [
     path('/men/', views.CategoryViews.as_view(), name="category-men"),
-    path('/men/<slug:product_slug>', views.ProductViews.as_view(), name="category-men"),
+    path('/men/<slug:product_slug>/', views.ProductViews.as_view(), name="category-men"),
 ]
